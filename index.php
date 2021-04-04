8909ab3a516cc31897abb6e31841da0dde8ef5c1
SolDevelop <64288070+SolDevelop@users.noreply.github.com>
2021-04-04 08:12:12 -0700

    MacOS Beta Support !
    Now You Can Control your mac in this new beta

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 if (isset($_GET['run'])){
 $V = $_GET['run'];
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'){
 $E = shell_exec("python main.py -load " . $V);
 echo $E;
 }else {
-echo "put the launching point"
-
+$E = shell_exec("python3 main.py -load " . $V);
+echo $E;
+}
 }
 ?>
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import threading
 import json
 oss = platform.system()
 import pathlib
-file = pathlib.Path("setup.txt")
+file = pathlib.Path("setup.key")
 import ctypes
 import webbrowser
 import signal
@@ -31,7 +31,7 @@ if oss == "Windows":
                 print("If You Want To Donate! Go To https://www.paypal.me/soldeveloperm")
                 os.system("netsh advfirewall firewall add rule name=\"SHORTCUT 80\" dir=in action=allow protocol=TCP localport=80")
 
-                E = open("setup.txt", "w+")
+                E = open("setup.key", "w+")
                 E.write("" + oss)
                 E.close()
                 sys.exit()
@@ -42,17 +42,15 @@ if oss == "Windows":
             print("You Already Setuped it")
        elif sys.argv[1] == "-run":
 
-           
+
            layout = [[sg.Text('Please Enter The IP that the Server will be running on')],
     [sg.Text('IP Address:', size=(15, 1)), sg.InputText()],
     [sg.Submit(), sg.Cancel()],[sg.Text('If You Want To Donate! Go To https://www.paypal.me/soldeveloperm')]]
            window = sg.Window(title="RPCC", layout=layout, margins=(100, 50))
 
-           
+
            while True:
                event, values = window.read()
-               
-
                if event == "Cancel" or event == sg.WIN_CLOSED:
                    window.close()
                    exit()
@@ -64,19 +62,16 @@ if oss == "Windows":
                      if __name__ == '__main__':
                         t = "php -S " + values[0] + ":80"
                         proc = subprocess.Popen(t, shell=False)
-                        run = open("run.key", "r+")
-                        run.truncate(0)
+                        os.remove("run.key")
+                        run = open("run.key", "w+")
                         run.write("R")
                         run.close()
                         evente = windowd.read()
                         windowd.close()
                         if evente == "Exit" or evente == sg.WIN_CLOSED:
-                             run = open("run.key", "r+")
-                             text = run.read()
-                             run.seek(0)
-                             text = text.replace("R", "N")
-                             run.write(text)
-                             run.truncate()
+                             os.remove("run.key")
+                             run = open("run.key", "w+")
+                             run.write("N")
                              run.close()
                              sys.exit()
                              break
@@ -87,18 +82,78 @@ if oss == "Windows":
            run = open("run.key", "r")
            if run.readline() == "R":
                WTO = sys.argv[2]
-               CV = key
+               CV = key[str(oss)][0]
                if WTO in CV:
                    Run = CV[str(WTO)]
                    subprocess.call([Run])
-                   print("Done! " + WTO + " Is Running!")
+                   print("Done! " + WTO  + " Is Running!")
            else:
-               print("The Server Isnt Running?")
+               print("The Server Isnt Running?, You Seems sus!")
 
 
 
 
 elif oss == "Darwin":
-         print("Sorry " + oss +" Isnt Supported")
+         if sys.argv[1] == "-setup":
+             if not file.exists():
+                 print("If You Want To Donate! Go To https://www.paypal.me/soldeveloperm")
+                 os.system("sudo nc -l 80")
+
+                 E = open("setup.key", "w+")
+                 E.write("Setup Completed\n OS: " + oss)
+                 E.close()
+                 sys.exit
+             else:
+                 print("You Already Setuped it")
+         elif sys.argv[1] == "-run":
+
+             layout = [[sg.Text('Please Enter The IP that the Server will be running on')],
+                       [sg.Text('IP Address:', size=(15, 1)), sg.InputText()],
+                       [sg.Submit(), sg.Cancel()],
+                       [sg.Text('If You Want To Donate! Go To https://www.paypal.me/soldeveloperm')]]
+             window = sg.Window(title="RPCC", layout=layout, margins=(100, 50))
+
+             while True:
+                 event, values = window.read()
+                 if event == "Cancel" or event == sg.WIN_CLOSED:
+                     window.close()
+                     exit()
+                     break
+                 elif event == "Submit":
+                     window.close()
+                     layouts = [[sg.Text("Server is running")], [sg.Button("Exit")]]
+                     windowd = sg.Window("RPCC", layouts)
+                     if __name__ == '__main__':
+                         t = "sudo php -S " + values[0] + ":80"
+                         proc = subprocess.Popen(t, stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE, shell=True)
+                         os.remove("run.key")
+                         run = open("run.key", "w+")
+                         run.write("R")
+                         run.close()
+                         evente = windowd.read()
+                         windowd.close()
+                         if evente == "Exit" or evente == sg.WIN_CLOSED:
+                             os.remove("run.key")
+                             run = open("run.key", "w+")
+                             run.write("N")
+                             run.close()
+                             os.system("kill " + proc.pid)
+                             sys.exit()
+                             break
+         elif sys.argv[1] == "-load":
+             with open('load.key', 'rt') as load:
+                 for line in load:
+                     key = json.loads(line)
+             run = open("run.key", "r")
+             if run.readline() == "R":
+                 WTO = sys.argv[2]
+                 CV = key[str(oss)][0]
+                 if WTO in CV:
+                     Run = CV[str(WTO)]
+                     os.system("open " + Run)
+                     print("Done! " + WTO + " Is Running!")
+             else:
+                 print("The Server Isnt Running?, You Seems sus!")
 elif oss == "Linux":
          print("Sorry " + oss +" Isnt Supported")
